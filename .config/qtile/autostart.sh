24afce3cd6c9b1335b4ef7a0470ddc0b33169701
Praneet Kumar <39919187+praneetk96@users.noreply.github.com>
2021-05-08 21:46:23 +0530

    Add files via upload
    My autostart bash script to start applications when qtile runs

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+picom &
+nitrogen --restore &
+gnome-keyring &
+gnome-keyring-3 &
+gnome-keyring-daemon &
+mailspring -b &
+telegram-desktop -startintray &
+xfce4-power-manager &
+nm-applet &
+xfce4-clipman &
