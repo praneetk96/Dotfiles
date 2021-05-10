0603738e3175f1275cbd14e4140d853f333d162c
Praneet Kumar <39919187+praneetk96@users.noreply.github.com>
2021-05-10 15:46:24 +0530

    Add files via upload
    Added XF86 keys in the config

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+picom &
+nitrogen --restore &
+gnome-keyring &
+gnome-keyring-3 &
+gnome-keyring-daemon &
+mailspring -b &
+telegram-desktop -startintray &
+nm-applet &
+xfce4-clipman &
