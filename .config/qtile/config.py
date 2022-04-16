34c245e412e62fa11c58c82d47beb8a19941ec3f
Praneet Kumar <39919187+praneetk96@users.noreply.github.com>
2022-04-16 07:08:53 +0530

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -329,4 +329,4 @@ def start_once():
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D
+wmname = "LG3D"
